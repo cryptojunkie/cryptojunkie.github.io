273698315bd5180388e2e92ea4f0908e59814faf
cryptojunkie <cryptojunkieapp@gmail.com>
2017-07-05 23:51:15 -0700

    Add files via upload

--- cryptojunkie-builder/js/app.js
+++ cryptojunkie-builder/js/app.js
@@ -2,6 +2,7 @@ var alertApi = "http://cryptojunkie.io/alerts.json";
 var linkApi = "http://cryptojunkie.io/links.json";
 var imageUrl = "http://api.screenshotlayer.com/api/capture?access_key=72cfbd767bdf2b0a7a26dc1b6ace791f&viewport=1280x1024&width=1280&fullpage=1&delay=5&ttl=604800&url=";
 var tmpLinks = [];
+var tmpAlerts = [];
 
 $(function() {
 
@@ -9,6 +10,7 @@ $(function() {
   $(document).on("click", ".js-add-link", addLink);
   $(document).on("click", ".js-output", clickOutput);
   $(document).on("change", ".js-toggle-link", toggleLink);
+  // $(document).on("change", ".js-toggle-alert", toggleAlert);
   $(document).on("change", ".js-data-source", changeDataSource);
 
   // start the process
@@ -79,17 +81,18 @@ $(function() {
     // update ui  
     $('.js-grid').prepend(helperLinkView(link.url));
     $('.js-new-link').val('');
-
-    // copy json to clipboard
-    document.execCommand(JSON.stringify(tmpLinks));
   }
 
   function toggleLink(event){
-    alert('not working yet');
-    // link = {url: $(event.currentTarget).data('link')};
-    // todo: need logic here for knowing if the checkbox was turned on or off
-    // tmpLinks = _.without(tmpLinks, link);
-    // rendorJSONOutput(tmpLinks);
+    tmpLinks = [];
+    $('.cj-link:checkbox:checked').each(function(){
+      tmpLinks.push({"url":$(this).data('link')})
+    });
+    rendorJSONOutput(tmpLinks);
+  }
+
+  function toggleAlert(event){
+    // to do
   }
 
   function renderLinks(links){
@@ -101,7 +104,12 @@ $(function() {
   }
 
   function renderAlerts(alerts){
-    // todo
+    tmpAlerts = alerts;
+    $('.js-grid').empty();
+    $.each(alerts, function(i, alert) {
+      $('.js-grid').append(helperAlertView(i,alert));
+    });
+    rendorJSONOutput(tmpAlerts);
   }
 
   function rendorJSONOutput(json){
@@ -109,7 +117,11 @@ $(function() {
   }
 
   function helperLinkView(link){
-    return '<tr class="link"><td><input type="checkbox" class="js-toggle-link" checked id="' + link + '" data-link="' + link + '" /> <label for="' +  link + '">' + link + '</label><div style="margin-left: 23px; margin-top:4px"><a href="' + link + '" target="_blank">View URL</a> &nbsp; <a href="' + imageUrl + link + '" target="_blank">View generated image</a></div></td></tr>';
+    return '<tr class="link"><td><input type="checkbox" class="cj-link js-toggle-link" checked id="' + link + '" data-link="' + link + '" /> <label for="' +  link + '">' + link + '</label><div style="margin-left: 23px; margin-top:4px"><a href="' + link + '" target="_blank">View URL</a> &nbsp; <a href="' + imageUrl + link + '" target="_blank">View generated image</a></div></td></tr>';
+  }
+
+  function helperAlertView(i,alert){
+    return '<tr class="alert"><td><input type="checkbox" class="cj-alert js-toggle-alert" checked id="' + i + '" data-alert="' + alert.message + '" /> <label for="' +  i + '">' + alert.message + '</label></td></tr>';
   }
 
   function clickOutput(){
